d51f32404c4db34e77c67dc931a7379739cff7ce
Karolina S <karolina.sk31@gmail.com>
2018-05-27 10:24:52 +0200

    My second commit message

--- src/Nowy.java
+++ src/Nowy.java
@@ -3,5 +3,10 @@ public class Nowy {
     public static void main (String [] args){
 
         System.out.println("Witaj w drugim projekcie ");
+
+        System.out.println("Druga wiadomosc");
     }
+
+
 }
+
